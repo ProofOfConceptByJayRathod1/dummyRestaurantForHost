1c6b4455d76cac46215682754cbf4b4aa288939d
Jay Rathod <jay.rathod@simformsolutions.com>
2022-04-03 02:22:45 +0530

    host this 5

--- src/main/java/com/simformsolutions/restaurant/controller/RestaurantController.java
+++ src/main/java/com/simformsolutions/restaurant/controller/RestaurantController.java
@@ -2,6 +2,8 @@ package com.simformsolutions.restaurant.controller;
 
 import java.util.List;
 
+import javax.persistence.criteria.Order;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,16 +16,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.simformsolutions.restaurant.model.Customer;
 import com.simformsolutions.restaurant.model.Restaurant;
+import com.simformsolutions.restaurant.model.Orders;
+import com.simformsolutions.restaurant.model.Menu;
+
+import com.simformsolutions.restaurant.model.DiningTable;
+import com.simformsolutions.restaurant.model.Feedback;
+import com.simformsolutions.restaurant.repository.FeedbackRepo;
+import com.simformsolutions.restaurant.repository.MenuRepo;
+import com.simformsolutions.restaurant.repository.OrdersRepo;
+
 import com.simformsolutions.restaurant.repository.RestaurantRepo;
+import com.simformsolutions.restaurant.repository.TableRepo;
+
 
 @RestController
 public class RestaurantController {
 	
 	@Autowired
 	private RestaurantRepo restaurantRepo; 
-	
+	@Autowired
+	private TableRepo tableRepo; 
+	@Autowired
+	private MenuRepo menuRepo; 
+	@Autowired
+	private OrdersRepo ordersRepo;	
+	@Autowired
+	private FeedbackRepo feedbackRepo;	
 	//get list of Restaurants only
-	@RequestMapping(value = "/allRestaurant" ,method = RequestMethod.GET)
+	@RequestMapping(value = "/allrestaurant" ,method = RequestMethod.GET)
 	public  ResponseEntity<List<Restaurant>> allRestaurant(){
 		
 		return new ResponseEntity<List<Restaurant>>(restaurantRepo.findAll() , HttpStatus.OK);
@@ -38,12 +58,43 @@ public class RestaurantController {
 		
 	}
 	
+	@RequestMapping(value = "/settabledetails" ,method = RequestMethod.POST)
+	public ResponseEntity<DiningTable> postTableDetails(@RequestBody DiningTable diningTable)
+	{
+		//return new ResponseEntity<Restaurant>(restaurantRepo.findTablesById(restaurant.getRestaurantId()),HttpStatus.OK);
+		return new ResponseEntity<DiningTable>(tableRepo.save(diningTable),  HttpStatus.CREATED);
+		
+	}
+	
+	@RequestMapping(value = "/addmenu" ,method = RequestMethod.POST)
+	public ResponseEntity<Menu> menuPost(@RequestBody Menu menu)
+	{
+		//return new ResponseEntity<Restaurant>(restaurantRepo.findTablesById(restaurant.getRestaurantId()),HttpStatus.OK);
+		return new ResponseEntity<Menu>(menuRepo.save(menu),  HttpStatus.CREATED);
+		
+	}
+	
+	@RequestMapping(value = "/addorder" ,method = RequestMethod.POST)
+	public ResponseEntity<Orders> ordersPost(@RequestBody Orders orders)
+	{
+		//return new ResponseEntity<Restaurant>(restaurantRepo.findTablesById(restaurant.getRestaurantId()),HttpStatus.OK);
+		return new ResponseEntity<Orders>(ordersRepo.save(orders),  HttpStatus.CREATED);
+		
+	}
+	
+	@RequestMapping(value = "/feedback" ,method = RequestMethod.POST)
+	public ResponseEntity<Feedback> feedbackPost(@RequestBody Feedback feedback)
+	{
+		//return new ResponseEntity<Restaurant>(restaurantRepo.findTablesById(restaurant.getRestaurantId()),HttpStatus.OK);
+		return new ResponseEntity<Feedback>(feedbackRepo.save(feedback),  HttpStatus.CREATED);
+		
+	}
+	
 	
 	@RequestMapping(value = "/addrestaurant" ,method = RequestMethod.POST)
 	public ResponseEntity<Restaurant> postRestaurantData(@RequestBody Restaurant restaurant)
 	{
 		
-		;
 		//return new ResponseEntity<Restaurant>(restaurantRepo.findTablesById(restaurant.getRestaurantId()),HttpStatus.OK);
 		return new ResponseEntity<Restaurant>(restaurantRepo.save(restaurant),  HttpStatus.CREATED);
 		
