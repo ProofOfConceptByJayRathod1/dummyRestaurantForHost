291763576c02e41476a0b51abe0e781d75d71bb7
Jay Rathod <jay.rathod@simformsolutions.com>
2022-04-02 22:54:59 +0530

    initial host for checkup

---
+++ src/main/java/com/simformsolutions/restaurant/model/Customer.java
@@ -0,0 +1,65 @@
+package com.simformsolutions.restaurant.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long customerId;
+	private String name;
+	private String email;
+	private String password;
+
+	//One Customer Has Many Feedbacks
+	@OneToMany(targetEntity = Feedback.class, cascade = CascadeType.ALL)
+	@JoinColumn(name = "fkCustomerId", referencedColumnName = "customerId")
+	private List<Feedback> customerFeedbacks;
+
+	@OneToMany(targetEntity = Orders.class, cascade = CascadeType.ALL)
+	@JoinColumn(name = "fkCustomerId", referencedColumnName = "customerId")
+	private List<Orders> customerOrders;
+	
+	
+	public long getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(long customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
---
+++ src/main/java/com/simformsolutions/restaurant/model/Menu.java
@@ -0,0 +1,67 @@
+package com.simformsolutions.restaurant.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Menu{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long menuId;
+	private String dishName;
+	private String dishType;
+	private int price;
+	private int buyFrequency;
+
+	public Menu(long menuId, String dishName, String dishType, int price, int buyFrequency) {
+		this.menuId = menuId;
+		this.dishName = dishName;
+		this.dishType = dishType;
+		this.price = price;
+		this.buyFrequency=buyFrequency;
+	}
+
+	public long getMenuId() {
+		return menuId;
+	}
+
+	public int getBuyFrequency() {
+		return buyFrequency;
+	}
+
+	public void setBuyFrequency(int buyFrequency) {
+		this.buyFrequency = buyFrequency;
+	}
+
+	public void setMenuId(long menuId) {
+		this.menuId = menuId;
+	}
+
+	public String getDishName() {
+		return dishName;
+	}
+
+	public void setDishName(String dishName) {
+		this.dishName = dishName;
+	}
+
+	public String getDishType() {
+		return dishType;
+	}
+
+	public void setDishType(String dishType) {
+		this.dishType = dishType;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+}
