d86b96b01af730edc3d47538fa2cf4be731fc819
Jay Rathod <jay.rathod@simformsolutions.com>
2022-04-03 10:36:48 +0530

    host this 14

--- src/main/java/com/simformsolutions/restaurant/controller/LoginController.java
+++ src/main/java/com/simformsolutions/restaurant/controller/LoginController.java
@@ -4,7 +4,10 @@ package com.simformsolutions.restaurant.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.simformsolutions.restaurant.model.Customer;
 import com.simformsolutions.restaurant.repository.CustomerRepo;
@@ -19,19 +22,14 @@ public class LoginController {
 	private CustomerRepo customerRepo;
 
 	@PostMapping("/login")
-//	public ResponseEntity<String> LoginPost (@RequestBody Customer customer)
-	public ResponseEntity<String> LoginPost (@RequestParam("email") String email,@RequestParam("password") String password )
+	public ResponseEntity<String> LoginPost (@RequestBody Customer customer)
 	{
-		Customer customer1= customerRepo.findByEmail(email);
-		if(customer1 != null && (password.equals(customer1.getPassword())))
+		
+		if(customerRepo.findByEmail(customer.getEmail()) != null && customerRepo.findByPassword(customer.getPassword()) != null)
 		{
-			return new ResponseEntity<String>("this" + email + "and" + password + "matched", HttpStatus.OK);
+			return new ResponseEntity<String>("Login Successfull..!!", HttpStatus.OK);
 		}
-		else
-		{
-			return new ResponseEntity<String>("this" + email + "and" + password + "did not matched", HttpStatus.UNAUTHORIZED);
-		}
-
+		return new ResponseEntity<String>("Wrong login credentials.", HttpStatus.UNAUTHORIZED);	
 		
 	}
 	
